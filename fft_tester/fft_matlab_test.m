198583b409b4bcb1105583d9264a0291a136ed14
Rijesh Augustine <rijesh@ualberta.ca>
2016-05-23 14:39:40 -0600

    Prelim_FFT_blockFP works with matlab and utilizes a hanning windows

--- fft_tester/fft_matlab_test.m
+++ fft_tester/fft_matlab_test.m
@@ -6,7 +6,7 @@ fs =1024;
 T=1/fs;
 n = 0:10000;
 t=n*T;
-x=sin(2*pi*f*t);
+x=10000*sin(2*pi*f*t);
 
 data = [n;x]';
 
@@ -21,4 +21,12 @@ f = [0:N-1] * delta_f;
 im = Data_im.data;
 re = Data_re.data;
 Mag = sqrt(im.^2 +re.^2);
-plot([0:255],20*log10(Mag(9571:9826)))
+figure;
+plot([0:255],Mag(9571:9826))
+
+
+%%
+ %alternate plot
+ 
+plot(Data_ind.data,re)
+ 
